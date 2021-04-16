cb0d122978b9e2d8ab2a3a64b5f094ef90d5334b
Richard Shea <rshea@thecubagroup.com>
2021-04-16 15:37:06 +1200

    Define a todo type and make use of it with some dummy data in a fixed length list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem';
+
+const todos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
+
 
 function App() {
-    return <>Hello World</>;
+    return (
+        <>
+            <TodoListItem todo={todos[0]} />
+            <TodoListItem todo={todos[1]} />
+        </>
+    )
 }
 
 export default App;
